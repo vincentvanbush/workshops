8880882d65548bc84793fb0eae177b98311d1750
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2014-11-01 16:19:14 +0100

    Task 4, 5, 6: model specs fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,14 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, :presence => true, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
+
+  def average_rating
+  	avg = 0.0
+  	reviews.each { |r| avg += r.rating }
+  	avg /= reviews.count
+  end
 end
