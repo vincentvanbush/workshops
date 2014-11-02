1e49a55b4d11da9da0b9e9ea3335fe2195837a0e
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2014-11-02 22:36:35 +0100

    Product edit and delete links visible only to product owner

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,7 @@ class ProductsController < ApplicationController
 
   def create
     self.product = Product.new(product_params)
+    product.user = current_user
 
     if product.save
       category.products << product
