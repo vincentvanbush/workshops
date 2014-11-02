e3b12ac54d56a548eb1fbbcbd50242220b7826a0
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2014-11-02 22:03:05 +0100

    Task 9: assigning the right user to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,6 +8,7 @@ class ReviewsController < ApplicationController
 
   def create
     self.review = Review.new(review_params)
+    review.user = current_user
 
     if review.save
       product.reviews << review
