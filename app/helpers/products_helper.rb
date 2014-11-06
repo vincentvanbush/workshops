ca537a2fd259207087c62a81d81a8e3eb3fb2632
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2014-11-06 12:26:03 +0100

    Revamped category product index view

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,12 @@
 module ProductsHelper
+
+  def product_link(product, action)
+    if action == :show
+      link_to raw('<span class="glyphicon glyphicon-folder-open"></span>&nbsp;Show'), category_product_path(product.category, product)
+    elsif action == :edit
+      link_to raw('<span class="glyphicon glyphicon-edit"></span>'), edit_category_product_path(product.category, product)
+    elsif action == :destroy
+      link_to raw('<span class="glyphicon glyphicon-remove"></span>'), category_product_path(product.category, product), method: :delete, data: { confirm: 'Are you sure?' }
+    end
+  end
 end
