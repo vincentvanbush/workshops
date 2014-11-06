7435a71b66730f230abce54d9ec836962cb6308f
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2014-11-06 12:47:31 +0100

    Product list shows avg rating and formatted price

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,12 +1,22 @@
 module ProductsHelper
 
   def product_link(product, action)
-    if action == :show
-      link_to raw('<span class="glyphicon glyphicon-folder-open"></span>&nbsp;Show'), category_product_path(product.category, product)
-    elsif action == :edit
+    if action == :edit
       link_to raw('<span class="glyphicon glyphicon-edit"></span>'), edit_category_product_path(product.category, product)
     elsif action == :destroy
       link_to raw('<span class="glyphicon glyphicon-remove"></span>'), category_product_path(product.category, product), method: :delete, data: { confirm: 'Are you sure?' }
     end
+  end
+
+  def product_avg_rating_formatted(product)
+    if not product.average_rating.nan?
+      number_with_precision(product.average_rating, precision: 2)
+    else
+      'No ratings'
+    end
+  end
+
+  def product_price_formatted(product)
+    number_to_currency(product.price)
   end
 end
